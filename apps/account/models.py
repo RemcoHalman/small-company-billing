2730d304acf7ed5bc5fd2c8137d63e74fab64aea
RemcoHalman <remco.halman@gmail.com>
2021-12-27 17:23:25 +0100

    feat: custom user admin added

---
+++ apps/account/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(max_length=40, blank=True, null=True)
+    second_name = models.CharField(max_length=40, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
---
+++ apps/core/management/commands/current_time.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from datetime import datetime
+from pytz import timezone
+from tzlocal import get_localzone
+
+now_utc = datetime.now(timezone('UTC'))
+now_local = now_utc.astimezone(get_localzone())
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+        time = now_local.strftime('%X')
+        self.stdout.write(self.style.SUCCESS("\nIt's now %s\n" % time))
